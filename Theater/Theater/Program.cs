ffdd12a5d7dac4cf2819c6ee7cdeda98a1487f3a
Дмитрий Баканов <dima.bakanov@mail.ru>
2018-12-20 01:25:44 +0300

    added output

--- Theater/Theater/Program.cs
+++ Theater/Theater/Program.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Theater
 {
    
     
     class Program
-        { public class Person
         {
-            public string name;
-            public string surname;
-            public Person(string _name, string _surname)
+        public class Person
             {
-                name = _name;
-                surname = _surname;
+                public string name;
+                public string surname;
+                public Person(string _name, string _surname)
+                {
+                    name = _name;
+                    surname = _surname;
+                }
+                public string output()
+                {
+                    return name + " " + surname;
+                }
             }
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Enter name");
@@ -26,8 +32,7 @@ namespace Theater
             Console.WriteLine("Enter surname");
             string surname = Console.ReadLine();
             Person user = new Person(name, surname);
-            Console.WriteLine(user.surname);
-            Console.WriteLine(user.name);
+            user.output();
             Console.ReadKey();
         }
     }
